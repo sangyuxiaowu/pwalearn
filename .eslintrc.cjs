fba8277d4f293490c1a377391649e79684e78aaa
sang <sang93@qq.com>
2022-08-08 13:47:34 +0800

    添加 PWA 支持

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,4 +9,11 @@ module.exports = {
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
   ],
+  globals: {
+    process: true, // fix 'process' is not defined no-undef
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
 };
---
+++ PWA.md
@@ -0,0 +1,5 @@
+# 安装 PWA
+
+安装命令为 `vue add pwa`
+
+> 注意，之前依赖安装需要使用 `npm i`，若使用 cnpm 会报错：`npm ERR! Unsupported URL Type "workspace:": workspace:*`
