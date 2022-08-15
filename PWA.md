364f7e6b632743d46a322101a82b6b1c9d644c23
sang <sang93@qq.com>
2022-08-15 18:40:16 +0800

    增加配置详情

--- PWA.md
+++ PWA.md
@@ -17,11 +17,6 @@ npx http-server dist
 # 相关网站
 
 - https://vite-plugin-pwa.netlify.app/
-- https://dev.to/wtho/custom-service-worker-logic-in-typescript-on-vite-4f27
 - https://pwa.nuxtjs.org/
 - https://cli.vuejs.org/core-plugins/pwa.html
-- https://juejin.cn/post/6844903971065167885
-- https://juejin.cn/post/7039258299086143524
-- https://medium.com/@stephen.trevor.wong/3-steps-to-add-pwa-to-vue-js-3a352a969d58
-- 应用图像生成 https://www.pwabuilder.com/imageGenerator
-- Vue CLI 创建 PWA https://blog.logrocket.com/building-pwa-vue/
+- https://www.pwabuilder.com/imageGenerator
--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,44 @@ export default defineConfig({
     vue(),
     vueJsx(),
     VitePWA({
+      manifest: {
+        name: "PWA 学习",
+        description: "我的第一个 PWA 项目",
+        theme_color: "#00bd7e",
+        icons: [
+          {
+            src: "/App_icon192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/App_icon512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/App_icon60.png",
+            sizes: "60x60",
+            type: "image/png",
+          },
+        ],
+        shortcuts: [
+          {
+            name: "Open About",
+            short_name: "About",
+            description: "Open the about page",
+            url: "/about",
+            icons: [{ src: "/App_icon192.png", sizes: "192x192" }],
+          },
+          {
+            name: "Report issue",
+            short_name: "Report",
+            description: "Open the issue report page",
+            url: "/report",
+            icons: [{ src: "/App_icon192.png", sizes: "192x192" }],
+          },
+        ],
+      },
       registerType: "autoUpdate",
       devOptions: {
         enabled: true,
